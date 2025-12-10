7f0f9d8b385c1da8eb8725c569dc423742f55aad
Stacy Crampton <stacyann6284@gmail.com>
2025-12-10 16:04:26 -0500

    styling

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, "app")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/day/:day", async (req, res) => {
   const day = req.params.day;
